60889bacf204874a5d2d460cc70c6d296566dde5
ash-kaian <muntashir.ash@gmail.com>
2024-06-02 02:57:25 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:app_mbatik/repositories/auth_repo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'bloc/login/login_cubit.dart';
+import 'bloc/register/register_cubit.dart';
 import 'core/constants/colors.dart';
 import 'core/router/app_router.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,34 +25,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
     final router = appRouter.router;
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-        textTheme: GoogleFonts.dmSansTextTheme(
-          Theme.of(context).textTheme,
-        ),
-        appBarTheme: AppBarTheme(
-          color: AppColors.white,
-          titleTextStyle: GoogleFonts.quicksand(
-            color: AppColors.primary,
-            fontSize: 18.0,
-            fontWeight: FontWeight.w700,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(create: (context) => RegisterCubit()),
+      ],
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+          textTheme: GoogleFonts.dmSansTextTheme(
+            Theme.of(context).textTheme,
           ),
-          iconTheme: const IconThemeData(
-            color: AppColors.black,
-          ),
-          centerTitle: true,
-          shape: Border(
-            bottom: BorderSide(
-              color: AppColors.black.withOpacity(0.05),
+          appBarTheme: AppBarTheme(
+            color: AppColors.white,
+            titleTextStyle: GoogleFonts.quicksand(
+              color: AppColors.primary,
+              fontSize: 18.0,
+              fontWeight: FontWeight.w700,
+            ),
+            iconTheme: const IconThemeData(
+              color: AppColors.black,
+            ),
+            centerTitle: true,
+            shape: Border(
+              bottom: BorderSide(
+                color: AppColors.black.withOpacity(0.05),
+              ),
             ),
           ),
         ),
+        routerDelegate: router.routerDelegate,
+        routeInformationParser: router.routeInformationParser,
+        routeInformationProvider: router.routeInformationProvider,
       ),
-      routerDelegate: router.routerDelegate,
-      routeInformationParser: router.routeInformationParser,
-      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
